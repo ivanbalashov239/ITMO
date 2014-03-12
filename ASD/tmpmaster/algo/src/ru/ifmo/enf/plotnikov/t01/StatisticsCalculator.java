600cc2a4e3ab2807b17ffa00d3b2594b3c10d695
plotnikovanton <plotnikovanton@gmail.com>
2014-03-12 22:22:26 +0400

    Submodule ASD

--- ASD/tmpmaster/algo/src/ru/ifmo/enf/plotnikov/t01/StatisticsCalculator.java
+++
@@ -1,5 +0,0 @@
-package ru.ifmo.enf.plotnikov.t01;
-
-public interface StatisticsCalculator<E extends Comparable<E>> {
-    E getStatistics(int k);
-}
--- ASD/tmpmaster/algo/src/ru/ifmo/enf/plotnikov/t04/ProbablyPrimeCalculator.java
+++
@@ -1,5 +0,0 @@
-package ru.ifmo.enf.plotnikov.t04;
-
-public interface ProbablyPrimeCalculator {
-    boolean isProbablyPrime(long n);
-}
