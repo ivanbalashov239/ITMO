5b28c57d9cefbbc2135bae47f0eb2f11dcdb69d3
plotnikovanton <plotnikovanton@gmail.com>
2014-11-19 00:03:55 +0300

    Task_1_M: NumberDate

---
+++ TP/Java/Task_1_M/src/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+    public static void main(String[] args) {
+        SimpleDate date1 = new NumbersDate(SimpleDate.Month.JANUARY, 21 ,2013);
+        SimpleDate date2 = new NumbersDate(SimpleDate.Month.FEBRUARY, 23 ,2013);
+
+        System.out.println(date2.from(date1));
+
+        date1 = new NumbersDate(SimpleDate.Month.JANUARY, 1 ,2001);
+        date2 = new NumbersDate(SimpleDate.Month.NOVEMBER, 19 ,2014);
+
+        System.out.println(date2.from(date1));
+
+        date1 = new NumbersDate(SimpleDate.Month.NOVEMBER, 1 ,2001);
+        date2 = new NumbersDate(SimpleDate.Month.JANUARY, 19 ,2014);
+
+        System.out.println(date2.from(date1));
+    }
+}
